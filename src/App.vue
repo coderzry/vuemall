2fb3df5bbe70092052aeb5f4f98b72cb693feca4
coderzry <18348738308@163.com>
2020-10-17 17:19:20 +0800

    首页开发完成

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="wrapper">
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar
+    }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 事件总线
+Vue.prototype.$bus = new Vue()
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
